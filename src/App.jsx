d077ed435b0be3dddcef29f84ca1a21e7956d2d4
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2023-07-04 03:03:59 +0200

    if video can't play condition added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect, useRef } from "react";
 import Header from "./components/Header";
-import Hero from "./components/Hero"
+import Hero from "./components/Hero";
 import Acomplishements from "./components/Acomplishements";
 import Project from "./components/Project";
 
 function App() {
   const videoRef = useRef(null);
+  const [videoError, setVideoError] = useState(false);
 
   useEffect(() => {
     const video = videoRef.current;
@@ -16,30 +17,48 @@ function App() {
     }, 2000);
   }, []);
 
+  const handleVideoError = () => {
+    setVideoError(true);
+  };
+
   return (
     <>
-      
-      <video
-        ref={videoRef}
-        autoPlay
-        muted
-        loop
-        id="myVideo"
-        style={{
-          position: "fixed",
-          right: "0",
-          bottom: "0",
-          minWidth: "100%",
-          minHeight: "100%",
-          zIndex: "-1",
-          opacity:0.9,
-        }}
-      >
-       <source src="/Video/BackgroundVid2.mp4" type="video/mp4" />
-      </video>
+      {!videoError ? (
+        <video
+          ref={videoRef}
+          autoPlay
+          muted
+          loop
+          id="myVideo"
+          style={{
+            position: "fixed",
+            right: "0",
+            bottom: "0",
+            minWidth: "100%",
+            minHeight: "100%",
+            zIndex: "-1",
+            opacity: 0.9,
+          }}
+          onError={handleVideoError}
+        >
+          <source src="/Video/BackgrundVid2.mp4" type="video/mp4" />
+        </video>
+      ) : (
+        <div
+          style={{
+            position: "fixed",
+            right: "0",
+            bottom: "0",
+            minWidth: "100%",
+            minHeight: "100%",
+            backgroundColor: "black",
+            zIndex: "-1",
+          }}
+        ></div>
+      )}
       <Header />
       <Hero />
-      <Project/>
+      <Project />
       <Acomplishements />
     </>
   );
