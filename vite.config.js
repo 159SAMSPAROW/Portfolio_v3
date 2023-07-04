c4e12dfadc7a5529b0bdf6158babd2d2428d3192
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2023-07-04 02:00:55 +0200

    vite.config updated

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['styled-normalize'],
-    },
-    optimizeDeps: {
-      include: ['styled-normalize']
-    }
-  },
+
 })
